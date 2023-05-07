ad6640a12d98ad76a289c9cf44e0987d94994a86
Sergei089 <ermolenkosereza3@gmail.com>
2023-05-07 11:56:18 +0300

    homework

---
+++ show/ex10.py
@@ -0,0 +1,16 @@
+d_artagnan = "ХТМеня зовут д'Артаньян."
+athos = "Эта строка\празделена на две."
+porthos = "Я \\ - \\ мушкетер!"
+
+aramis = """
+Подсказки:
+\t* Граф из Гаскони
+\t* Связан с Миледи
+\t* BapoH\n\t* Генерал Ордена
+
+"""
+
+print (d_artagnan)
+print (athos)
+print (porthos)
+print (aramis)
