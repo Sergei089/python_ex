ff791d9d9a1afb8cb5774139ba43fceba8fdc400
Serg <ermolenkosereza3@gmail.com>
2023-06-07 20:21:11 +0300

    stepik alg 2.1 max var1

---
+++ stepik/ЕГЭ 2.1.py
@@ -0,0 +1,19 @@
+num = "23 7"
+def number(num):
+	num = num.split()
+	num2 = []
+
+	for i in num:
+		num2.append(i)
+
+	# if num2[0] > num2[1]:
+		# print(num2[0])
+	if 23 > 7:
+		print(23)
+number(num)
+
+
+
+
+# перевести в число
+# 
